38d93adc93d9119db357d91b65ffd99f69a32617
david-zhou <david.zhou.tan@gmail.com>
2015-02-06 20:46:42 -0600

    LAVAAAAAAA

--- main.lua
+++ main.lua
@@ -1,18 +1,78 @@
-bg = display.newImage('bg.jpg')
---Fonts
---[[Gargle = Font.load("gargle.ttf")
-Gargle:setPixelSizes(14,14)
-GargleIt = Font.load("gargleit.ttf")
-GargleIt:setPixelSizes(14,14)
-]]
---Title
-ttl = display.newImage('title.png')
-ttl:translate(display.contentCenterX, 60)
-ttl2 = display.newImage('title2.png')
-ttl2:translate(display.contentCenterX+2,56)
---Sprite
-ab = display.newImage('angrybird.gif')
-ab:translate(display.contentCenterX,170)
-ab:scale(1.5,1.5)
---Rate
-rt = display.newText("RATE", display.contentCenterX, 250, "GargleCdRg-Regular", 40)
+--
+-- Abstract: Composer Sample
+--
+-- Version: 1.0
+-- 
+-- Sample code is MIT licensed, see http://www.coronalabs.com/links/code/license
+-- Copyright (C) 2011 Corona Labs Inc. All Rights Reserved.
+--
+-- Demonstrates use of the Composer API (scene events, transitioning, etc.)
+--
+-- Supports Graphics 2.0
+------------------------------------------------------------
+
+-- hide device status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- require controller module
+local composer = require "composer"
+
+local widget = require "widget"
+
+local gameNetwork = require( "gameNetwork" )
+local playerName
+
+local function loadLocalPlayerCallback( event )
+   playerName = event.data.alias
+   saveSettings()  --save player data locally using your own "saveSettings()" function
+end
+
+local function gameNetworkLoginCallback( event )
+   gameNetwork.request( "loadLocalPlayer", { listener=loadLocalPlayerCallback } )
+   return true
+end
+
+local function gpgsInitCallback( event )
+   gameNetwork.request( "login", { userInitiated=true, listener=gameNetworkLoginCallback } )
+end
+
+local function gameNetworkSetup()
+   if ( system.getInfo("platformName") == "Android" ) then
+      gameNetwork.init( "google", gpgsInitCallback )
+   else
+      gameNetwork.init( "gamecenter", gameNetworkLoginCallback )
+   end
+end
+
+local function systemEvents( event )
+   print("systemEvent " .. event.type)
+   if ( event.type == "applicationSuspend" ) then
+      print( "suspending..........................." )
+   elseif ( event.type == "applicationResume" ) then
+      print( "resuming............................." )
+   elseif ( event.type == "applicationExit" ) then
+      print( "exiting.............................." )
+   elseif ( event.type == "applicationStart" ) then
+      gameNetworkSetup()  --login to the network here
+   end
+   return true
+end
+
+Runtime:addEventListener( "system", systemEvents )
+
+-- load first scene
+options = {
+	effect = 'fade',
+	time = 400,
+	params = 
+	{
+
+	}
+}
+composer.gotoScene( "menu", options )
+
+--
+-- Display objects added below will not respond to composer transitions
+--
+
+-- table to setup tabBar buttons
